c65e155b6e8261d1eeb36bad209b504befeb1130
“John <vasyl@example.com>
2026-05-05 20:36:38 +0300

    practice

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,4 @@
-from pywebio.input import input as input_pw
+from pywebio.input import input as input_pw, NUMBER, slider, checkbox
 from pywebio.output import put_markdown, put_success, put_error, put_image, put_warning, put_text, put_row, put_column
 import messages_rest
 import images
@@ -19,7 +19,12 @@ put_row(
 )
 put_row(
     [
-        put_column([put_image(images.COLA_IMAGE, width='300px', height='200px'), put_text(f'Кола {prices.COLA_PRICE}')]),
-        put_column([put_image(images.CAUSE_IMAGE, width='300px', height='200px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
+        put_column([put_image(images.COLA_IMAGE, width='180px', height='120px'), put_text(f'Кола {prices.COLA_PRICE}')]),
+        put_column([put_image(images.CAUSE_IMAGE, width='180px', height='120px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
     ]
 )
+pizza_quantity = input_pw(label='Pizza', type=NUMBER, value=1)
+burger_quantity = slider(label='Burger', min_value=0, max_value=10, value=2)
+causes = checkbox(label='Causes', options=['майонез', 'кетчуп', 'гірчиця'])
+print(causes)
+causes_quantity = len(causes)
