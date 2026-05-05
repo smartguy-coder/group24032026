1e6ecff1f45877311db60de6c1e2ba60bba19251
“John <vasyl@example.com>
2026-05-05 20:06:04 +0300

    practice

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,8 @@
 from pywebio.input import input as input_pw
-from pywebio.output import put_markdown, put_success, put_error, put_warning
+from pywebio.output import put_markdown, put_success, put_error, put_image, put_warning, put_text, put_row, put_column
 import messages_rest
+import images
+import prices
 
 guest_name = input_pw(messages_rest.MSG_WELCOME_GUEST, required=True)
 put_success(messages_rest.MSG_WELCOME_GUEST_WITH_NAME.format(name=guest_name))
@@ -9,5 +11,15 @@ put_success(messages_rest.MSG_WELCOME_GUEST_WITH_NAME.format(name=guest_name))
 
 put_markdown("## Меню")
 
-print(guest_name)
-
+put_row(
+    [
+        put_column([put_image(images.PIZZA_IMAGE, width='300px', height='200px'), put_text(f'Піцца {prices.PIZZA_PRICE}')]),
+        put_column([put_image(images.BURGER_IMAGE, width='300px', height='200px'), put_text(f'Бургер {prices.BURGER_PRICE}')]),
+    ]
+)
+put_row(
+    [
+        put_column([put_image(images.COLA_IMAGE, width='300px', height='200px'), put_text(f'Кола {prices.COLA_PRICE}')]),
+        put_column([put_image(images.CAUSE_IMAGE, width='300px', height='200px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
+    ]
+)
