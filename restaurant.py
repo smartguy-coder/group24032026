a4eca208bbf9f8b5f5e50ec4bc76e4a815977755
“John <vasyl@example.com>
2026-05-05 20:52:55 +0300

    practice

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,4 @@
-from pywebio.input import input as input_pw, NUMBER, slider, checkbox
+from pywebio.input import input as input_pw, NUMBER, slider, checkbox, select
 from pywebio.output import put_markdown, put_success, put_error, put_image, put_warning, put_text, put_row, put_column
 import messages_rest
 import images
@@ -13,18 +13,43 @@ put_markdown("## Меню")
 
 put_row(
     [
-        put_column([put_image(images.PIZZA_IMAGE, width='300px', height='200px'), put_text(f'Піцца {prices.PIZZA_PRICE}')]),
-        put_column([put_image(images.BURGER_IMAGE, width='300px', height='200px'), put_text(f'Бургер {prices.BURGER_PRICE}')]),
+        put_column(
+            [put_image(images.PIZZA_IMAGE, width='300px', height='200px'), put_text(f'Піцца {prices.PIZZA_PRICE}')]),
+        put_column(
+            [put_image(images.BURGER_IMAGE, width='300px', height='200px'), put_text(f'Бургер {prices.BURGER_PRICE}')]),
+        put_column(
+            [put_image(images.COLA_IMAGE, width='180px', height='120px'), put_text(f'Кола {prices.COLA_PRICE}')]),
+        put_column(
+            [put_image(images.CAUSE_IMAGE, width='180px', height='120px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
     ]
 )
-put_row(
-    [
-        put_column([put_image(images.COLA_IMAGE, width='180px', height='120px'), put_text(f'Кола {prices.COLA_PRICE}')]),
-        put_column([put_image(images.CAUSE_IMAGE, width='180px', height='120px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
-    ]
-)
+# put_row(
+#     [
+#         put_column(
+#             [put_image(images.COLA_IMAGE, width='180px', height='120px'), put_text(f'Кола {prices.COLA_PRICE}')]),
+#         put_column(
+#             [put_image(images.CAUSE_IMAGE, width='180px', height='120px'), put_text(f'Соуси {prices.CAUSE_PRICE}')]),
+#     ]
+# )
+
+# get quantity
 pizza_quantity = input_pw(label='Pizza', type=NUMBER, value=1)
 burger_quantity = slider(label='Burger', min_value=0, max_value=10, value=2)
 causes = checkbox(label='Causes', options=['майонез', 'кетчуп', 'гірчиця'])
-print(causes)
 causes_quantity = len(causes)
+
+cola = select("Cola", options=['-', 'класична', 'light'])
+cola_quantity = 0
+if cola != '-':
+    cola_quantity = 1
+
+# calculate total prices
+total_cost_pizza = pizza_quantity * prices.PIZZA_PRICE
+total_cost_burger = burger_quantity * prices.BURGER_PRICE
+total_cost_cola = cola_quantity * prices.COLA_PRICE
+total_cost_causes = cola_quantity * prices.CAUSE_PRICE
+
+total_cost = total_cost_cola + total_cost_burger + total_cost_pizza + total_cost_causes
+
+
+put_warning()
